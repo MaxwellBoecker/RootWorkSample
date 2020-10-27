4cddf9e208fbaf578c76e5de482dc2499bc80fae
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-10-27 18:06:43 -0500

    (feat) finished helpers, program in index.js produces result

---
+++ helpers.js
@@ -0,0 +1,64 @@
+const getDrivers = (driversAndTrips) => driversAndTrips.filter((string) => string.split(' ').length === 2);
+
+const getTrips = (driversAndTrips) => driversAndTrips.filter((string) => string.split(' ').length === 5);
+
+const storeDrivers = (drivers) => drivers.reduce((obj, cur) => {
+  const driver = cur.split(' ')[1];
+  obj[driver] = {};
+  return obj;
+}, {});
+
+/**
+ * @param {array} trips this is the array of strings (trips) from the input file
+ * @return {array} an array of objects (trips turned into objects)
+ */
+const tripParser = (trips) => trips.map((tripString) => {
+  const trip = tripString.split(' ').slice(1);
+  const minutesAtStart = ((trip[1].split(':')[0] * 1) * 60) + (trip[1].split(':')[1] * 1);
+  const minutesAtEnd = ((trip[2].split(':')[0] * 1) * 60) + (trip[2].split(':')[1] * 1);
+  const hours = (minutesAtEnd - minutesAtStart) / 60;
+  const distance = trip[3] * 1;
+
+  return {
+    driver: trip[0],
+    distance,
+    hours,
+  };
+});
+
+const pruneTrips = (trips) => trips.filter((trip) => {
+  const mph = trip.distance / trip.hours;
+  if (mph >= 5 && mph <= 100) {
+    return trip;
+  }
+});
+
+const tripAggregator = (prunedTrips, driversObj) => prunedTrips.reduce((accumulator, trip) => {
+  const driverStats = accumulator[trip.driver];
+  if (driverStats.distance) {
+    driverStats.distance += trip.distance;
+    driverStats.hours += trip.hours;
+  } else {
+    driverStats.distance = trip.distance;
+    driverStats.hours = trip.hours;
+  }
+  return accumulator;
+}, driversObj);
+
+const createResult = (aggregatedTrips) => Object.entries(aggregatedTrips).map((stats) => {
+  if (stats[1].distance !== undefined) {
+    const mph = stats[1].distance / stats[1].hours;
+    return `${stats[0]}: ${Math.round(stats[1].distance)} miles @ ${Math.round(mph)} mph`;
+  }
+  return `${stats[0]}: 0 miles`;
+}).sort((a, b) => b.split(' ')[1] - a.split(' ')[1]).join('\n');
+
+module.exports = {
+  getDrivers,
+  getTrips,
+  storeDrivers,
+  tripParser,
+  pruneTrips,
+  tripAggregator,
+  createResult,
+};
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('yello');
+const fs = require('fs');
+const {
+  getDrivers,
+  getTrips,
+  storeDrivers,
+  tripParser,
+  pruneTrips,
+  tripAggregator,
+  createResult,
+} = require('./helpers.js');
+
+// The variable driversAndTrips is the result of reading the input file
+// from the command line. I turn it into an array of strings with split()
+const driversAndTrips = fs.readFileSync(0).toString().split('\n');
+
+// The variable trips is the result of filtering driversAndTrips to only include trips
+const trips = getTrips(driversAndTrips);
+
+// The variable drivers is the result of filtering driversAndTrips to only include drivers
+const drivers = getDrivers(driversAndTrips);
+
+// driversObj is an object that contains a key for each driver and
+// an empty object stored at each key. It will be fed to tripAggregator.
+const driversObj = storeDrivers(drivers);
+
+// The variable parsedTrips is an array of objects which contain pertinent data
+// about trips; 'trip objects'
+const parsedTrips = tripParser(trips);
+
+// The variable prunedTrips is an array of 'trip objects' with trips with average speed above
+// 100 mph or below 5 mph filtered out.
+const prunedTrips = pruneTrips(parsedTrips);
+
+// The variable aggregatedTrips is the result of combining driversObj with the prunedTrips.
+// This allows us to aggregate the data if a driver has multiple trips
+// and more easily compute the average speed. It is an object of objects.
+const aggregatedTrips = tripAggregator(prunedTrips, driversObj);
+
+// This is the final product! Turns the aggregatedTrips obj back into a string!
+const result = createResult(aggregatedTrips);
+
+// const result = createResult(tripAggregator(pruneTrips(tripParser(trips)), driversObj));
+
+console.log(result);
